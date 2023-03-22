71644d866951b9620b1435d7fab21b055bb37b58
TupiNUMBooR <TupiNUMBooR@gmail.com>
2023-03-22 12:49:21 +1000

    Init

---
+++ src/test/java/ru/tupi/lunchvoting/LunchVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tupi.lunchvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
